a6a53fd91e61910ec6b017af7646cbb54f53bd9e
florian-lemesle <florian.lemesle@laplateforme.io>
2023-11-30 16:29:13 +0100

    addsub ok

--- addition.h
+++ addition.h
@@ -1,5 +1,5 @@
 void addition(int n1, int n2) {
 
-    printf("Le résultat de la multiplication de %d et %d est : %d\n", n1, n2, n1+n2);
+    printf("Le résultat de l'addition de %d et %d est : %d\n", n1, n2, n1+n2);
 
 }
---
+++ soustraction.h
@@ -0,0 +1,5 @@
+void soustraction(int n1, int n2) {
+
+    printf("Le résultat de la soustraction de %d et %d est : %d\n", n1, n2, n1-n2);
+
+}
