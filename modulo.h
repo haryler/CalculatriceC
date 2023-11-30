536e1228ad93e83c6eb936772d819511a1540d9d
yannick-cousin1 <113202561+yannick-cousin1@users.noreply.github.com>
2023-11-30 16:34:50 +0100

    add \n missing at the end

--- modulo.h
+++ modulo.h
@@ -1,5 +1,5 @@
 
 void modulo(int n1, int n2)
 {
-	printf("Le quotient entier de %d par %d vaut %d et le reste de la division entière de %d par %d vaut %d" , n1, n2, n1/n2, n1, n2, n1%n2);
+	printf("Le quotient entier de %d par %d vaut %d et le reste de la division entière de %d par %d vaut %d\n" , n1, n2, n1/n2, n1, n2, n1%n2);
 }
